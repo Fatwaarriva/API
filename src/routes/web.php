b4041e730119a1b826b658b61f443d24def7b5e3
Fatwaarriva <fatwaarrivaaa@gmail.com>
2024-07-29 20:27:39 +0700

    arriva

--- src/routes/web.php
+++ src/routes/web.php
@@ -16,7 +16,7 @@ $router->get('/', function () use ($router) {
     return $router->app->version();
 });
 
-$router->group(['prefix' => 'api/v1/user', 'middleware'=>'auth'], function() use ($router){
+$router->group(['prefix' => 'api/v1/users', 'middleware'=>'auth'], function() use ($router){
 	$router->get('/', ['uses' => 'UserController@index']);
 });
 
